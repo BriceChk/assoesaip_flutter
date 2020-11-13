45357f54d263f50ea81384562e88f5c183d4533f
Brice Chkir <brice.chkir@outlook.fr>
2020-11-13 11:58:48 +0100

    Updated icon names

--- lib/shares/navBar.dart
+++ lib/shares/navBar.dart
@@ -79,7 +79,7 @@ class _NavigationBarState extends State<NavigationBar> {
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Home',
+            label: 'Accueil',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_today_rounded),
@@ -87,7 +87,7 @@ class _NavigationBarState extends State<NavigationBar> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.group),
-            label: 'Club & Asso',
+            label: 'Clubs & assos',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.fastfood),
