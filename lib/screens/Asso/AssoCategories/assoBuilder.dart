9afc63da3384db60745427e834181a7669061ab8
Madere0 <59331413+Madere0@users.noreply.github.com>
2020-11-21 18:35:56 +0100

    Filling the builder with the nested navigation

--- lib/screens/Asso/AssoCategories/assoBuilder.dart
+++ lib/screens/Asso/AssoCategories/assoBuilder.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'assoCategories.dart';
+import 'assoSubCategories.dart';
+
 class AssoBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Navigator(
+      initialRoute: 'AssociationCategories',
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case 'AssociationCategories':
+            return MaterialPageRoute(
+                builder: (context) => AssociationCategories(),
+                settings: settings);
+            break;
+
+          case 'Test':
+            return MaterialPageRoute(
+                builder: (context) => Test(), settings: settings);
+            break;
+
+          default:
+            throw Exception("Invalid route");
+        }
+      },
+    );
   }
 }
