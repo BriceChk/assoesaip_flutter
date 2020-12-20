cd4ffd9e086c4bc77b9b9e9e79fc07c7e4eac10e
nicomont <43388212+nicomont@users.noreply.github.com>
2020-12-20 16:15:13 +0100

    Page cafet v1
    Page cafet, implémentation repas, boissons, desserts.
    Ajout d'image dans la BDD.

--- lib/screens/Cafet/cafet.dart
+++ lib/screens/Cafet/cafet.dart
@@ -1,7 +1,6 @@
 import 'package:assoesaip_flutter/shares/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:sumup/sumup.dart';
 
 final String classicFont = "Nunito";
 final Color cardColor = white;
@@ -47,7 +46,35 @@ class _CafetCategoriesState extends State<CafetCategories> {
           toolbarHeight: 130,
           pinned: true,
           backgroundColor: hearderColor,
-        )
+        ),
+        //* We wrap the rest of the page inside the SliverList: like this everything scrool vertically except the header
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              SizedBox(
+                height: 5,
+              ),
+              //* Widget with all cards for meals, drinks and desserts
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Liste des repas'),
+              ),
+              BodyLayout(),
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Liste des boissons'),
+              ),
+              Boisson(),
+              TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none, hintText: 'Liste des desserts'),
+              ),
+              Dessert(),
+              //* Sizedbox of height 60 because otherwise the last one is under the navbar
+              SizedBox(height: 60),
+            ],
+          ),
+        ),
       ],
     );
   }
@@ -86,94 +113,281 @@ class Header extends StatelessWidget {
   }
 }
 
-final String affiliateKey = 'your-affiliate-key';
+class BodyLayout extends StatelessWidget {
+  final List<List<String>> repasList = [
+    [
+      "Repas 1",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas1.jpeg"
+    ],
+    [
+      "Repas 2",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas2.jpg"
+    ],
+    [
+      "Repas 3",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas3.jpg"
+    ],
+    [
+      "Repas 4",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas4.jpg"
+    ],
+    [
+      "Repas 5",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas5.jpg"
+    ],
+    [
+      "Repas 6",
+      "Composant 1, Composant 2, ....",
+      "assets/images/Cafet/Repas/repas6.jpg"
+    ],
+  ];
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Sumup plugin'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              onPressed: () async {
-                var init = await Sumup.init(affiliateKey);
-                print(init);
-              },
-              child: Text('Init'),
+  @override
+  Widget build(BuildContext context) {
+    final repasMap = repasList.asMap();
+    return Column(
+      //* Display the cafet menu
+      children: repasMap
+          .map(
+            (i, element) => MapEntry(
+              i,
+              //* Here we're building each card with each name of the specific meal
+              Padding(
+                padding: EdgeInsets.only(top: 5, right: 10, left: 10),
+                child: Container(
+                  //! boxConstraints like this we can set a min height to the card and combine with flexible the height can be override
+                  constraints: BoxConstraints(
+                    minHeight: 90,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 0.5,
+                    shadowColor: shadowColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: cardColor,
+                    //* InkWell like this we can integrate the ontap function
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(10),
+                      splashColor: splashColor,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 7.5,
+                          vertical: 5,
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 75,
+                              child: Image.asset(element[2]),
+                            ),
+                            SizedBox(width: 5),
+                            //! Wrap in flexible like this we can have text in multiline
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  //* Title of card
+                                  Text(
+                                    element[0],
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: classicFont,
+                                        color: titleColor),
+                                  ),
+                                  //* Description of the card
+                                  Text(
+                                    element[1],
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontFamily: classicFont,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            FlatButton(
-              onPressed: () async {
-                var login = await Sumup.login();
-                print(login);
-              },
-              child: Text('Login'),
-            ),
-            FlatButton(
-              onPressed: () async {
-                var settings = await Sumup.openSettings();
-                print(settings);
-              },
-              child: Text('Open settings'),
-            ),
-            FlatButton(
-              onPressed: () async {
-                var payment = SumupPayment(
-                  title: 'Test payment',
-                  total: 1.2,
-                  currency: 'EUR',
-                  foreignTransactionId: '',
-                  saleItemsCount: 0,
-                  skipSuccessScreen: false,
-                  tip: .0,
-                );
+          )
+          .values
+          .toList(),
+    );
+  }
+}
 
-                var request = SumupPaymentRequest(payment, info: {
-                  'AccountId': 'taxi0334',
-                  'From': 'Paris',
-                  'To': 'Berlin',
-                });
+class Boisson extends StatelessWidget {
+  final List<List<String>> boissonsList = [
+    ["Coca", "assets/images/Cafet/Boissons/coca.png"],
+    ["Fanta", "assets/images/Cafet/Boissons/fanta.jpg"],
+    ["S.Pellegrino", "assets/images/Cafet/Boissons/SanPe.png"],
+    ["Sprite", "assets/images/Cafet/Boissons/Sprite.png"],
+  ];
 
-                var checkout = await Sumup.checkout(request);
-                print(checkout);
-              },
-              child: Text('Checkout'),
+  @override
+  Widget build(BuildContext context) {
+    final boissonsMap = boissonsList.asMap();
+    return Column(
+      //* Display the drinks menu
+      children: boissonsMap
+          .map(
+            (i, element) => MapEntry(
+              i,
+              //* Here we're building each card with each name of the specific drink
+              Padding(
+                padding: EdgeInsets.only(top: 5, right: 10, left: 10),
+                child: Container(
+                  //! boxConstraints like this we can set a min height to the card and combine with flexible the height can be override
+                  constraints: BoxConstraints(
+                    minHeight: 90,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 0.5,
+                    shadowColor: shadowColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: cardColor,
+                    //* InkWell like this we can integrate the ontap function
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(10),
+                      splashColor: splashColor,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 7.5,
+                          vertical: 5,
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 75,
+                              child: Image.asset(element[1]),
+                            ),
+                            SizedBox(width: 5),
+                            //! Wrap in flexible like this we can have text in multiline
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  //* Title of card
+                                  Text(
+                                    element[0],
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: classicFont,
+                                        color: titleColor),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            FlatButton(
-              onPressed: () async {
-                var isLogged = await Sumup.isLoggedIn;
-                print(isLogged);
-              },
-              child: Text('Is logged in'),
+          )
+          .values
+          .toList(),
+    );
+  }
+}
+
+class Dessert extends StatelessWidget {
+  final List<List<String>> dessertsList = [
+    ["Fincancier x3", "assets/images/Cafet/Desserts/dessert1.jpg"],
+    ["Cookies x2", "assets/images/Cafet/Desserts/dessert2.jpg"],
+    ["Brownie", "assets/images/Cafet/Desserts/dessert3.jpg"],
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final dessertsMap = dessertsList.asMap();
+    return Column(
+      //* Display the desserts menu
+      children: dessertsMap
+          .map(
+            (i, element) => MapEntry(
+              i,
+              //* Here we're building each card with each name of the specific dessert
+              Padding(
+                padding: EdgeInsets.only(top: 5, right: 10, left: 10),
+                child: Container(
+                  //! boxConstraints like this we can set a min height to the card and combine with flexible the height can be override
+                  constraints: BoxConstraints(
+                    minHeight: 90,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Card(
+                    elevation: 0.5,
+                    shadowColor: shadowColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: cardColor,
+                    //* InkWell like this we can integrate the ontap function
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(10),
+                      splashColor: splashColor,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 7.5,
+                          vertical: 5,
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 75,
+                              child: Image.asset(element[1]),
+                            ),
+                            SizedBox(width: 5),
+                            //! Wrap in flexible like this we can have text in multiline
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  //* Title of card
+                                  Text(
+                                    element[0],
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontFamily: classicFont,
+                                        color: titleColor),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
-            FlatButton(
-              onPressed: () async {
-                var isInProgress = await Sumup.isCheckoutInProgress;
-                print(isInProgress);
-              },
-              child: Text('Is checkout in progress'),
-            ),
-            FlatButton(
-              onPressed: () async {
-                var merchant = await Sumup.merchant;
-                print(merchant);
-              },
-              child: Text('Current merchant'),
-            ),
-            FlatButton(
-              onPressed: () async {
-                var logout = await Sumup.logout();
-                print(logout);
-              },
-              child: Text('Logout'),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+          )
+          .values
+          .toList(),
+    );
+  }
 }
