a21132cb1012848b20eed527c3f41cef35560fc4
Brice Chkir <brice.chkir@outlook.fr>
2021-01-04 20:22:05 +0100

    Calendar events list

--- lib/models/eventOccurrence.dart
+++ lib/models/eventOccurrence.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:assoesaip_flutter/models/event.dart';
+
 EventOccurrence eventOccurrenceFromJson(String str) => EventOccurrence.fromJson(json.decode(str));
 
 String eventOccurrenceToJson(EventOccurrence data) => json.encode(data.toJson());
@@ -8,10 +10,12 @@ class EventOccurrence {
   EventOccurrence({
     this.id,
     this.date,
+    this.event,
   });
 
   int id;
   DateTime date;
+  Event event;
 
   factory EventOccurrence.fromJson(Map<String, dynamic> json) {
     if (json == null) return null;
@@ -19,11 +23,13 @@ class EventOccurrence {
     return EventOccurrence(
       id: json['id'],
       date: DateTime.parse(json['date']),
+      event: json['event'] == null ? null : Event.fromJson(json['event']),
     );
   }
 
   Map<String, dynamic> toJson() => {
     'id': id,
     'date': date.toIso8601String(),
+    'event': event == null ? null : event.toJson(),
   };
 }
