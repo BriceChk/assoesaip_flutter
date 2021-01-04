980d359a4b33b87b52a317dd765d978d548ceac2
Brice Chkir <brice.chkir@outlook.fr>
2021-01-04 22:43:45 +0100

    Refactoring

--- lib/screens/eventsPage.dart
+++ lib/screens/eventsPage.dart
@@ -33,6 +33,8 @@ class _EventPageState extends State<EventPage> {
     topRight: Radius.circular(10),
   );
 
+  DateFormat formatter = DateFormat("dd/MM/yyyy · HH'h'mm", 'fr_FR');
+
   @override
   void initState() {
     super.initState();
@@ -59,7 +61,7 @@ class _EventPageState extends State<EventPage> {
         backgroundColor: starCommandBlue,
       ),
       body: e is Event
-          ? _buildEventWidget()
+          ? _buildEventBody()
           : Column(
               children: [
                 Expanded(
@@ -75,95 +77,8 @@ class _EventPageState extends State<EventPage> {
     );
   }
 
-  Widget _buildEventWidget() {
-    DateFormat formatter = DateFormat("dd/MM/yyyy · HH'h'mm", 'fr_FR');
-    DateFormat formatterAllDay = DateFormat("EEEE dd MMM", 'fr_FR');
+  Widget _buildEventBody() {
     String date = formatter.format(e.datePublished.toLocal());
-    String dateStart;
-    String dateEnd;
-    List<String> occurenceList = [];
-
-    Widget _buildDate() {
-      if (e.occurrencesCount == 1) {
-        if (e.allDay) {
-          dateStart =
-              formatterAllDay.format(e.dateStart) + ', toute la journée';
-          dateEnd = formatterAllDay.format(e.dateEnd) + ', toute la journée';
-        } else {
-          dateStart = formatter.format(e.dateStart.toLocal());
-          dateEnd = formatter.format(e.dateEnd.toLocal());
-        }
-        if (dateStart == dateEnd) {
-          return ListTile(
-            leading: Icon(Icons.calendar_today),
-            title: Text(dateStart),
-            onTap: () => {},
-          );
-        } else {
-          return ListTile(
-            leading: Icon(Icons.calendar_today),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Début : ' + dateStart),
-                Text('Fin : ' + dateEnd)
-              ],
-            ),
-            onTap: () => {},
-          );
-        }
-      } else {
-        if (e.occurrences.length == 0) {
-          return Text("Toutes les dates sont passées");
-        } else {
-          if (e.allDay) {
-            for (var item in e.occurrences) {
-              occurenceList.add(formatterAllDay.format(item.date));
-            }
-            return Column(
-                children: occurenceList.map((occur) {
-              return ListTile(
-                leading: Icon(Icons.calendar_today),
-                title: Text(occur + ', toute la journée'),
-                onTap: () => {},
-              );
-            }).toList());
-          } else {
-            for (var item in e.occurrences) {
-              occurenceList.add(formatter.format(item.date));
-            }
-            //! Je sais pas comment faire pour ce cas la avec les minutes
-            //! et la méthode que j'ai fait !?
-            return Column(
-                children: occurenceList.map((occur) {
-              return ListTile(
-                leading: Icon(Icons.calendar_today),
-                title: Text(occur),
-                onTap: () => {},
-              );
-            }).toList());
-          }
-        }
-      }
-    }
-
-    void _showBottomSheet() {
-      showModalBottomSheet(
-        shape: RoundedRectangleBorder(borderRadius: bottomSheetBorderRadius),
-        context: context,
-        builder: (BuildContext bc) {
-          return ListView(children: [
-            Container(
-              child: Wrap(
-                children: <Widget>[
-                  _buildDate(),
-                ],
-              ),
-            ),
-          ]);
-        },
-      );
-    }
 
     return IndexedStack(index: _stackToView, children: [
       Scaffold(
@@ -298,11 +213,98 @@ class _EventPageState extends State<EventPage> {
     ]);
   }
 
+  Widget _buildDate() {
+    DateFormat formatterAllDay = DateFormat("EEEE dd MMM", 'fr_FR');
+    String dateStart;
+    String dateEnd;
+    List<String> occurenceList = [];
+
+    if (e.occurrencesCount == 1) {
+      if (e.allDay) {
+        dateStart =
+            formatterAllDay.format(e.dateStart) + ', toute la journée';
+        dateEnd = formatterAllDay.format(e.dateEnd) + ', toute la journée';
+      } else {
+        dateStart = formatter.format(e.dateStart.toLocal());
+        dateEnd = formatter.format(e.dateEnd.toLocal());
+      }
+      if (dateStart == dateEnd) {
+        return ListTile(
+          leading: Icon(Icons.calendar_today),
+          title: Text(dateStart),
+          onTap: () => {},
+        );
+      } else {
+        return ListTile(
+          leading: Icon(Icons.calendar_today),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Début : ' + dateStart),
+              Text('Fin : ' + dateEnd)
+            ],
+          ),
+          onTap: () => {},
+        );
+      }
+    } else {
+      if (e.occurrences.length == 0) {
+        return Text("Toutes les dates sont passées");
+      } else {
+        if (e.allDay) {
+          for (var item in e.occurrences) {
+            occurenceList.add(formatterAllDay.format(item.date));
+          }
+          return Column(
+              children: occurenceList.map((occur) {
+                return ListTile(
+                  leading: Icon(Icons.calendar_today),
+                  title: Text(occur + ', toute la journée'),
+                  onTap: () => {},
+                );
+              }).toList());
+        } else {
+          for (var item in e.occurrences) {
+            occurenceList.add(formatter.format(item.date));
+          }
+          //! Je sais pas comment faire pour ce cas la avec les minutes
+          //! et la méthode que j'ai fait !?
+          return Column(
+              children: occurenceList.map((occur) {
+                return ListTile(
+                  leading: Icon(Icons.calendar_today),
+                  title: Text(occur),
+                  onTap: () => {},
+                );
+              }).toList());
+        }
+      }
+    }
+  }
+
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      shape: RoundedRectangleBorder(borderRadius: bottomSheetBorderRadius),
+      context: context,
+      builder: (BuildContext bc) {
+        return ListView(children: [
+          Container(
+            child: Wrap(
+              children: <Widget>[
+                _buildDate(),
+              ],
+            ),
+          ),
+        ]);
+      },
+    );
+  }
+
   _loadHtmlFromString() async {
     String fileText = await rootBundle.loadString('assets/article.html');
     fileText = fileText.replaceFirst('%body%', e.html);
     _controller.loadUrl(Uri.dataFromString(fileText,
-            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
         .toString());
   }
 }
